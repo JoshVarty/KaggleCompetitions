3b9e6748da0fd088474881844a1d4973c910bc79
Josh Varty <joshvarty@gmail.com>
2017-09-28 23:11:38 -0400

    Initial structure

--- StreetViewHouseNumbers/StreetViewHouseNumbers/model1.py
+++ StreetViewHouseNumbers/StreetViewHouseNumbers/model1.py
@@ -40,8 +40,30 @@ train_images, valid_images, train_labels, valid_labels = train_test_split(train_
 
 
 def TrainConvNet(model_save_path):
+    
+    input = tf.placeholder(tf.float32, shape=(None, image_size, image_size, num_channels))
 
-    pass
+    w_conv1 = weight_layer("w_conv1", [patch_size_3, patch_size_3, num_channels, depth])
+    b_conv1 = bias_variable("b_conv1", [depth])
+    h_conv1 = conv2d_relu(input, w_conv1, b_conv1)
+    w_conv2 = weight_layer("w_conv2", [patch_size_3, patch_size_3, depth, depth])
+    b_conv2 = bias_variable("b_conv2", [depth])
+    h_conv2 = conv2d_relu(h_conv1, w_conv2, b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    w_conv3 = weight_layer("w_conv3", [patch_size_3, patch_size_3, depth, depth * 2])
+    b_conv3 = bias_variable("b_conv3", [depth * 2])
+    h_conv3 = conv2d_relu(h_pool2, w_conv3, b_conv3)
+    w_conv4 = weight_layer("w_conv4", [patch_size_3, patch_size_3, depth * 2, depth * 4])
+    b_conv4 = bias_variable("b_conv4", [depth * 4])
+    h_conv3 = conv2d_relu(h_conv3, w_conv4, b_conv4)
+    h_pool4 = max_pool_2x2(h_conv3)
+
+
+
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size, image_size, num_channels))
+    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
+    tf_valid_dataset = tf.constant(valid_dataset, dtype=tf.float32)
 
 def weight_layer(name, shape):
     return tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
@@ -49,8 +71,8 @@ def weight_layer(name, shape):
 def bias_variable(name, shape):
       return tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
 
-def conv2d(input, weights):
-    return tf.nn.conv2d(input, weights, [1,1,1,1], padding="SAME")
+def conv2d_relu(input, weights, bias):
+    return tf.nn.relu(tf.nn.conv2d(input, weights, [1,1,1,1], padding="SAME") + bias)
 
 def max_pool_2x2(input):    
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
