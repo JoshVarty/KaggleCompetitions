ead89e3276d2a7a1d5df77a1c214a9fbfbdb5aaf
Josh Varty <joshvarty@gmail.com>
2017-09-29 15:43:45 -0400

    Initial structure

--- StreetViewHouseNumbers/StreetViewHouseNumbers/model1.py
+++ StreetViewHouseNumbers/StreetViewHouseNumbers/model1.py
@@ -39,10 +39,12 @@ train_images, valid_images, train_labels, valid_labels = train_test_split(train_
 
 
 
-def TrainConvNet(model_save_path):
+def TrainConvNet(model_save_path, keep_prob):
     
     input = tf.placeholder(tf.float32, shape=(None, image_size, image_size, num_channels))
+    keep_prob = tf.placeholder(tf.float32)
 
+    #Conv->Relu->Conv->Relu->Pool
     w_conv1 = weight_layer("w_conv1", [patch_size_3, patch_size_3, num_channels, depth])
     b_conv1 = bias_variable("b_conv1", [depth])
     h_conv1 = conv2d_relu(input, w_conv1, b_conv1)
@@ -51,6 +53,7 @@ def TrainConvNet(model_save_path):
     h_conv2 = conv2d_relu(h_conv1, w_conv2, b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
+    #Conv->Relu->Conv->Relu->Pool
     w_conv3 = weight_layer("w_conv3", [patch_size_3, patch_size_3, depth, depth * 2])
     b_conv3 = bias_variable("b_conv3", [depth * 2])
     h_conv3 = conv2d_relu(h_pool2, w_conv3, b_conv3)
@@ -59,7 +62,31 @@ def TrainConvNet(model_save_path):
     h_conv3 = conv2d_relu(h_conv3, w_conv4, b_conv4)
     h_pool4 = max_pool_2x2(h_conv3)
 
+    #Conv->Relu->Conv->Relu->Conv->Relu->Pool
+    w_conv5 = weight_layer("w_conv5", [patch_size_3, patch_size_3, depth * 4, depth * 4])
+    b_conv5 = bias_variable("b_conv5", [depth * 4])
+    h_conv5 = conv2d_relu(h_pool4, w_conv4, b_conv4)
+    w_conv6 = weight_layer("w_conv6", [patch_size_3, patch_size_3, depth * 4, depth * 4])
+    b_conv6 = bias_variable("b_conv6", [depth * 4])
+    h_conv6 = conv2d_relu(h_conv5, w_conv6, b_conv6)
+    w_conv7 = weight_layer("w_conv7", [patch_size_3, patch_size_3, depth * 4, depth * 8])
+    b_conv7 = bias_variable("b_conv7", [depth * 8])
+    h_pool7 = max_pool_2x2(h_conv6)
 
+    #Dropout -> Fully Connected -> Dropout -> Fully Connected
+    drop_1 = tf.nn.dropout(h_pool7, keep_prob)
+    shape = drop_1.get_shape().as_list()
+    reshape = tf.reshape(drop_1, [shape[0], shape[1] * shape[2] * shape[3]])
+
+    fc = 234
+    w_fc_1 = weight_layer("w_fc_1", [fc, 4096])
+    b_fc_1 = bias_variable("b_fc_1", [4096])
+    hidden_1 = tf.matmul(reshape, w_fc_1) + b_fc_1
+
+    drop_2 = tf.nn.dropout(hidden_1, keep_prob)
+    w_fc_2 = weight_layer("w_fc_2", [4096, num_labels])
+    b_fc_2 = bias_variable("b_fc_2", [num_labels])
+    logits = tf.matmul(drop_2, w_fc_2) + b_fc_2
 
     tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size, image_size, num_channels))
     tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
