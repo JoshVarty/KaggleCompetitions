5374560d64cd64b582862ff71df46d163896d0a4
Josh Varty <joshvarty@gmail.com>
2017-09-30 22:08:37 -0400

    Use graph

--- StreetViewHouseNumbers/StreetViewHouseNumbers/model1.py
+++ StreetViewHouseNumbers/StreetViewHouseNumbers/model1.py
@@ -45,104 +45,106 @@ train_images, valid_images, train_labels, valid_labels = train_test_split(train_
 
 
 def TrainConvNet(model_save_path):
-    
-    input = tf.placeholder(tf.float32, shape=(None, image_size, image_size, num_channels))
-    labels = tf.placeholder(tf.float32, shape=(None, 5, 11))
-    keep_prob = tf.placeholder(tf.float32)
 
-    #Conv->Relu->Conv->Relu->Pool
-    w_conv1 = weight_layer("w_conv1", [patch_size_3, patch_size_3, num_channels, depth])
-    b_conv1 = bias_variable("b_conv1", [depth])
-    h_conv1 = conv2d_relu(input, w_conv1, b_conv1)
-    w_conv2 = weight_layer("w_conv2", [patch_size_3, patch_size_3, depth, depth])
-    b_conv2 = bias_variable("b_conv2", [depth])
-    h_conv2 = conv2d_relu(h_conv1, w_conv2, b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
+    graph = tf.Graph()
+    with graph.as_default():
+        input = tf.placeholder(tf.float32, shape=(None, image_size, image_size, num_channels))
+        labels = tf.placeholder(tf.float32, shape=(None, 5, 11))
+        keep_prob = tf.placeholder(tf.float32)
 
-    #Conv->Relu->Conv->Relu->Pool
-    w_conv3 = weight_layer("w_conv3", [patch_size_3, patch_size_3, depth, depth * 2])
-    b_conv3 = bias_variable("b_conv3", [depth * 2])
-    h_conv3 = conv2d_relu(h_pool2, w_conv3, b_conv3)
-    w_conv4 = weight_layer("w_conv4", [patch_size_3, patch_size_3, depth * 2, depth * 4])
-    b_conv4 = bias_variable("b_conv4", [depth * 4])
-    h_conv3 = conv2d_relu(h_conv3, w_conv4, b_conv4)
-    h_pool4 = max_pool_2x2(h_conv3)
+        #Conv->Relu->Conv->Relu->Pool
+        w_conv1 = weight_layer("w_conv1", [patch_size_3, patch_size_3, num_channels, depth])
+        b_conv1 = bias_variable("b_conv1", [depth])
+        h_conv1 = conv2d_relu(input, w_conv1, b_conv1)
+        w_conv2 = weight_layer("w_conv2", [patch_size_3, patch_size_3, depth, depth])
+        b_conv2 = bias_variable("b_conv2", [depth])
+        h_conv2 = conv2d_relu(h_conv1, w_conv2, b_conv2)
+        h_pool2 = max_pool_2x2(h_conv2)
 
-    #Conv->Relu->Conv->Relu->Conv->Relu->Pool
-    w_conv5 = weight_layer("w_conv5", [patch_size_3, patch_size_3, depth * 4, depth * 4])
-    b_conv5 = bias_variable("b_conv5", [depth * 4])
-    h_conv5 = conv2d_relu(h_pool4, w_conv5, b_conv5)
-    w_conv6 = weight_layer("w_conv6", [patch_size_3, patch_size_3, depth * 4, depth * 4])
-    b_conv6 = bias_variable("b_conv6", [depth * 4])
-    h_conv6 = conv2d_relu(h_conv5, w_conv6, b_conv6)
-    w_conv7 = weight_layer("w_conv7", [patch_size_3, patch_size_3, depth * 4, depth * 8])
-    b_conv7 = bias_variable("b_conv7", [depth * 8])
-    h_pool7 = max_pool_2x2(h_conv6)
+        #Conv->Relu->Conv->Relu->Pool
+        w_conv3 = weight_layer("w_conv3", [patch_size_3, patch_size_3, depth, depth * 2])
+        b_conv3 = bias_variable("b_conv3", [depth * 2])
+        h_conv3 = conv2d_relu(h_pool2, w_conv3, b_conv3)
+        w_conv4 = weight_layer("w_conv4", [patch_size_3, patch_size_3, depth * 2, depth * 4])
+        b_conv4 = bias_variable("b_conv4", [depth * 4])
+        h_conv3 = conv2d_relu(h_conv3, w_conv4, b_conv4)
+        h_pool4 = max_pool_2x2(h_conv3)
 
-    #Dropout -> Fully Connected -> Dropout -> Fully Connected
-    drop_1 = tf.nn.dropout(h_pool7, keep_prob)
-    shape = drop_1.get_shape().as_list()
-    reshape = tf.reshape(drop_1, [-1, shape[1] * shape[2] * shape[3]])
+        #Conv->Relu->Conv->Relu->Conv->Relu->Pool
+        w_conv5 = weight_layer("w_conv5", [patch_size_3, patch_size_3, depth * 4, depth * 4])
+        b_conv5 = bias_variable("b_conv5", [depth * 4])
+        h_conv5 = conv2d_relu(h_pool4, w_conv5, b_conv5)
+        w_conv6 = weight_layer("w_conv6", [patch_size_3, patch_size_3, depth * 4, depth * 4])
+        b_conv6 = bias_variable("b_conv6", [depth * 4])
+        h_conv6 = conv2d_relu(h_conv5, w_conv6, b_conv6)
+        w_conv7 = weight_layer("w_conv7", [patch_size_3, patch_size_3, depth * 4, depth * 8])
+        b_conv7 = bias_variable("b_conv7", [depth * 8])
+        h_pool7 = max_pool_2x2(h_conv6)
 
-    fc = 18432
-    w_fc_1 = weight_layer("w_fc_1", [fc, 4096])
-    b_fc_1 = bias_variable("b_fc_1", [4096])
-    h_fc_1 = tf.matmul(reshape, w_fc_1) + b_fc_1
+        #Dropout -> Fully Connected -> Dropout -> Fully Connected
+        drop_1 = tf.nn.dropout(h_pool7, keep_prob)
+        shape = drop_1.get_shape().as_list()
+        reshape = tf.reshape(drop_1, [-1, shape[1] * shape[2] * shape[3]])
 
-    drop_2 = tf.nn.dropout(h_fc_1, keep_prob)
-    w_fc_2 = weight_layer("w_fc_2", [4096, 11 * num_labels])
-    b_fc_2 = bias_variable("b_fc_2", [11 * num_labels])
-    h_fc_2 = tf.matmul(drop_2, w_fc_2) + b_fc_2
+        fc = 18432
+        w_fc_1 = weight_layer("w_fc_1", [fc, 4096])
+        b_fc_1 = bias_variable("b_fc_1", [4096])
+        h_fc_1 = tf.matmul(reshape, w_fc_1) + b_fc_1
+
+        drop_2 = tf.nn.dropout(h_fc_1, keep_prob)
+        w_fc_2 = weight_layer("w_fc_2", [4096, 11 * num_labels])
+        b_fc_2 = bias_variable("b_fc_2", [11 * num_labels])
+        h_fc_2 = tf.matmul(drop_2, w_fc_2) + b_fc_2
 
 
-    labels1 = tf.squeeze(tf.slice(labels, [-1, 0, 0], [-1, 1, 11]), axis=1)
-    labels2 = tf.squeeze(tf.slice(labels, [-1, 1, 0], [-1, 1, 11]), axis=1)
-    labels3 = tf.squeeze(tf.slice(labels, [-1, 2, 0], [-1, 1, 11]), axis=1)
-    labels4 = tf.squeeze(tf.slice(labels, [-1, 3, 0], [-1, 1, 11]), axis=1)
-    labels5 = tf.squeeze(tf.slice(labels, [-1, 4, 0], [-1, 1, 11]), axis=1)
+        labels1 = tf.squeeze(tf.slice(labels, [-1, 0, 0], [-1, 1, 11]), axis=1)
+        labels2 = tf.squeeze(tf.slice(labels, [-1, 1, 0], [-1, 1, 11]), axis=1)
+        labels3 = tf.squeeze(tf.slice(labels, [-1, 2, 0], [-1, 1, 11]), axis=1)
+        labels4 = tf.squeeze(tf.slice(labels, [-1, 3, 0], [-1, 1, 11]), axis=1)
+        labels5 = tf.squeeze(tf.slice(labels, [-1, 4, 0], [-1, 1, 11]), axis=1)
 
-    logits1 = tf.slice(h_fc_2, [-1, 0], [-1, 11])
-    logits2 = tf.slice(h_fc_2, [-1, 11], [-1, 11])
-    logits3 = tf.slice(h_fc_2, [-1, 22], [-1, 11])
-    logits4 = tf.slice(h_fc_2, [-1, 33], [-1, 11])
-    logits5 = tf.slice(h_fc_2, [-1, 44], [-1, 11])
+        logits1 = tf.slice(h_fc_2, [-1, 0], [-1, 11])
+        logits2 = tf.slice(h_fc_2, [-1, 11], [-1, 11])
+        logits3 = tf.slice(h_fc_2, [-1, 22], [-1, 11])
+        logits4 = tf.slice(h_fc_2, [-1, 33], [-1, 11])
+        logits5 = tf.slice(h_fc_2, [-1, 44], [-1, 11])
 
-    cost1 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels1, logits=logits1))
-    cost2 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels2, logits=logits2))
-    cost3 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels3, logits=logits3))
-    cost4 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels4, logits=logits4))
-    cost5 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels5, logits=logits5))
+        cost1 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels1, logits=logits1))
+        cost2 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels2, logits=logits2))
+        cost3 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels3, logits=logits3))
+        cost4 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels4, logits=logits4))
+        cost5 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels5, logits=logits5))
 
-    total_cost = cost1 + cost2 + cost3 + cost4 + cost5
+        total_cost = cost1 + cost2 + cost3 + cost4 + cost5
 
-    optimizer = tf.train.AdamOptimizer(0.00001).minimize(total_cost)
+        optimizer = tf.train.AdamOptimizer(0.00001).minimize(total_cost)
 
-    #Slice it out
-    #x = tf.slice(h_fc_2, [0,0,0], [-1, -1, - 1])
+        #Slice it out
+        #x = tf.slice(h_fc_2, [0,0,0], [-1, -1, - 1])
 
-    with tf.Session(graph=graph) as session:
-        num_steps = 10000
-        batch_size = 32
-        tf.global_variables_initializer().run()
-        print("Initialized")
+        with tf.Session(graph=graph) as session:
+            num_steps = 10000
+            batch_size = 32
+            tf.global_variables_initializer().run()
+            print("Initialized")
 
-        for step in range(num_steps):
-            offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
-            batch_data = train_images[offset:(offset + batch_size), :, :, :]
-            batch_labels = train_labels[offset:(offset + batch_size), :]
+            for step in range(num_steps):
+                offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+                batch_data = train_images[offset:(offset + batch_size), :, :, :]
+                batch_labels = train_labels[offset:(offset + batch_size), :]
 
-            feed_dict = {input : batch_data, labels : batch_labels}
+                feed_dict = {input : batch_data, labels : batch_labels}
 
-            if step % 500 == 0:
-                _, l, predictions, m = session.run([optimizer, loss, train_prediction, merged], feed_dict=feed_dict)
-                print('Minibatch loss at step %d: %f' % (step, l))
-                print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
-                #Validation
-                feed_dict = {input : valid_images, labels : valid_labels}
-                _, l, predictions, m = session.run([optimizer, loss, train_prediction, merged], feed_dict=feed_dict)
-                print('Valid accuracy: %.1f%%' % accuracy(predictions, valid_labels))
-            else:
-                _, l, predictions, m = session.run([optimizer, loss, train_prediction, merged], feed_dict=feed_dict)
+                if step % 500 == 0:
+                    _, l, predictions = session.run([optimizer, total_cost, train_prediction], feed_dict=feed_dict)
+                    print('Minibatch loss at step %d: %f' % (step, l))
+                    print('Minibatch accuracy: %.1f%%' % accuracy(predictions, batch_labels))
+                    #Validation
+                    feed_dict = {input : valid_images, labels : valid_labels}
+                    _, l, predictions,= session.run([optimizer, total_cost, train_prediction], feed_dict=feed_dict)
+                    print('Valid accuracy: %.1f%%' % accuracy(predictions, valid_labels))
+                else:
+                    _, l, predictions, m = session.run([optimizer, loss, train_prediction, merged], feed_dict=feed_dict)
 
 def weight_layer(name, shape):
     return tf.get_variable(name, shape, initializer=tf.contrib.layers.xavier_initializer())
